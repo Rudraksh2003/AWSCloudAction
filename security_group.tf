3e2a4586ee005e9294702e92bda8b80f9ef3431b
Rudraksh Laddha <77118726+Rudraksh2003@users.noreply.github.com>
2024-10-07 12:49:01 +0530

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "github_runner_sg" {
+  name        = "github-runner-sg"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with your IP range for security
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
